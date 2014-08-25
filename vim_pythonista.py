9a57817c2045b7500cb406914eaf04960ad3e721
Ryan <ryanfmurphy@gmail.com>
2014-08-25 01:41:54 -0400

    some comments
    so you guys can actually read my iphone-scrawled quasi-minified python code that I wrote on an airplane

--- vim_pythonista.py
+++ vim_pythonista.py
@@ -1,36 +1,37 @@
 import editor,re
 
-def i():
+def i(): # go into insert mode (so far this means nothing)
 	global vmode
 	vmode='insert'
-def a():
+def a(): # "Append", go into insert mode a space later
 	fw(1)
 	i()
 
-def instxt(pos,txt):
+def instxt(pos,txt): # insert text
 	editor.replace_text(pos,pos,txt)
 
+# shortcuts
 getsel = editor.get_selection
 getlsel = editor.get_line_selection
 
-def o():
+def o(): # open line below
 	end=getlsel()[1]
 	instxt(end,'\n')
 	mv(end)
 	i()
 	
-def O():
+def O(): # open line above
 	bgn=getlsel()[0]
 	instxt(bgn,'\n')
 	mv(bgn)
 	i()
 
-def csr():
+def csr(): # where is the cursor? return a byte/char number
 	return getsel()[0]
 
 def mvcsr(x): # move cursor
 	editor.set_selection(x,x)
-def fw(n):
+def fw(n): # move pythonista cursor forward n
 	mvcsr(csr()+n)
 def bk(n): fw(-n)
 
@@ -38,43 +39,43 @@ def fwtxt(): # txt after csr
 	return editor.get_text()[csr()+1:]
 def bktxt(): # txt b4 csr
 	return editor.get_text()[:csr()]
-def rbktxt():
+def rbktxt(): # txt b4 cursor reversed for backwards-searching
 	return ''.join(reversed(bktxt()))
 
-def fnd(regex,txt):
+def fnd(regex,txt): # shortcut for finding first regex position in txt
 	return re.search(regex,txt).start()
 
-def ch():
+def ch(): # current char at cursor
 	return editor.get_text()[csr()]
-def ws(ch): return ch in ' \t\n'
-def isw(ch):
+def ws(ch): return ch in ' \t\n' # is whitespace
+def isw(ch): # is word char
 	return bool(re.match(r'\w',ch))
 
-def Eamt(txt=None):
+def Eamt(txt=None): # how far to move for E command
 	if not txt: txt=fwtxt()
 	return fnd(r'[ \t\n]',txt)+1
 	#todo it should skip over ]', in the abv ln
-def eamt(txt=None):
+def eamt(txt=None): # how far for e
 	if not txt: txt=fwtxt()
 	return fnd(r'\W',txt)+1
-def wamt(txt=None):
+def wamt(txt=None): # once you go to end of word, how much further for next word?
 	if not txt: txt=fwtxt()
 	return fnd(r'\S',txt)+1
-def W():
+def W(): # move forward 1 WORD
 	fw(Eamt())
 	if ws(ch()): fw(wamt())
-def w():
+def w(): # move forward 1 word
 	#if isw(ch()):
 	fw(eamt())
 	#else: fw(1)
 	if ws(ch()): fw(wamt())
-def B(): #todo bB not working properly
+def B(): # move backwords 1 WORD #todo b/B not working properly
 	bk(Eamt(rbktxt()))
 	if ws(ch()): bk(wamt(rbktxt()))
-def b():
+def b(): # move backwards 1 word
 	bk(eamt(rbktxt()))
 	if ws(ch()): bk(wamt(rbktxt()))
-def E(): fw(Eamt())
-def e(): fw(eamt())
+def E(): fw(Eamt()) # end of WORD
+def e(): fw(eamt()) # end of word
 
 
